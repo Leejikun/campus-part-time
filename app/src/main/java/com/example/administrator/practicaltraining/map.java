3fd6007af23c0c13cce783e046a0949a826ee4b2
田保哲 <903087288@qq.com>
2016-11-30 09:06:53 +0800

    地图配置修改BUG成功，正在完善定位系统

--- app/src/main/java/com/example/administrator/practicaltraining/map.java
+++ app/src/main/java/com/example/administrator/practicaltraining/map.java
@@ -3,45 +3,30 @@ package com.example.administrator.practicaltraining;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.baidu.location.BDLocation;
-import com.baidu.location.BDLocationListener;
-import com.baidu.location.LocationClient;
-import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.BitmapDescriptor;
-import com.baidu.mapapi.map.MapStatusUpdate;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
-import com.baidu.mapapi.map.MyLocationData;
-import com.baidu.mapapi.model.LatLng;
 
 public class map extends AppCompatActivity {
-
     MapView mMapView = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         SDKInitializer.initialize(getApplicationContext());
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_map2);
         mMapView = (MapView) findViewById(R.id.bmapView);
     }
-
     @Override
     protected void onDestroy() {
         super.onDestroy();
 //  在activity 执行onDestroy 时执行mMapView.onDestroy() ，实现地图生命周期管理
         mMapView.onDestroy();
     }
-
     @Override
     protected void onResume() {
         super.onResume();
 //  在activity 执行onResume 时执行mMapView. onResume () ，实现地图生命周期管理
         mMapView.onResume();
     }
-
     @Override
     protected void onPause() {
         super.onPause();
