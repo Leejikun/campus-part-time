711a9acde246e7f9a189e9980c326554beb8c968
田保哲 <903087288@qq.com>
2016-12-01 09:45:44 +0800

    短信获取的alertdiaog实现   判断字符长度的实现，手机号码合法与否的实现

--- app/src/main/java/com/example/administrator/practicaltraining/ActivityLogin.java
+++ app/src/main/java/com/example/administrator/practicaltraining/ActivityLogin.java
@@ -1,17 +1,35 @@
 package com.example.administrator.practicaltraining;
 
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RemoteViews;
 import android.widget.TabHost;
+import android.widget.Toast;
+
+import com.baidu.platform.comapi.map.N;
 
 public class ActivityLogin extends AppCompatActivity {
      private EditText editText;
        private EditText editText1;
     private EditText editText2;
     private EditText editText3;
+    private Button button;
+    private EditText editText4;
+    private String string;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,7 +72,36 @@ public class ActivityLogin extends AppCompatActivity {
                 editText3.setText("");
             }
         });
+        button=(Button)findViewById(R.id.getpassword);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder AdBuilder = new AlertDialog.Builder(ActivityLogin.this);
+                AdBuilder.setIcon(R.drawable.comemessage);
+                AdBuilder.setTitle("温馨提示");
+                editText4=(EditText)findViewById(R.id.moblenumber);
+               String text=(editText4).getText().toString();
+                if(text.length()!=11){
+                    Toast.makeText(ActivityLogin.this,"请输入11位手机号",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(ActivityLogin.this,"输入合法",Toast.LENGTH_LONG).show();
+                    AdBuilder.setMessage("您确定给您的手机号为"+text+"发短信？");
+                    AdBuilder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+                    AdBuilder.setNegativeButton("取消", null);
+                    AdBuilder.create();
+                    AdBuilder.show();
+                }
+
+
+            }
+
+
+        });
     }
-
-
 }
