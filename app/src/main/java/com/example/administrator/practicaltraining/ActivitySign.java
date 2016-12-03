c3e89341b6790fff55c5b8b5b7d9a447020b195c
qinxinxin <1473020280@qq.com>
2016-12-03 21:07:41 +0800

    连接完成并报名

--- app/src/main/java/com/example/administrator/practicaltraining/ActivitySign.java
+++ app/src/main/java/com/example/administrator/practicaltraining/ActivitySign.java
@@ -2,6 +2,10 @@ package com.example.administrator.practicaltraining;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class ActivitySign extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class ActivitySign extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        //为“完成并报名”按钮绑定单击事件
+        Button BtChange = (Button)findViewById(R.id.id_btn);
+        BtChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //创建Toast
+                Toast toastTip = Toast.makeText(ActivitySign.this, "完成报名", 1000);
+                //设置Toast属性
+                toastTip.setGravity(Gravity.CENTER, 0, 0);
+                //显示Toast
+                toastTip.show();
+            }
+        });
     }
 }
