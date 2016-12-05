0aa40568f7001077802268b444a77bf943500e13
田保哲 <903087288@qq.com>
2016-12-05 17:31:36 +0800

    报名页部分问题的修改

--- app/src/main/java/com/example/administrator/practicaltraining/ActivityMessage.java
+++ app/src/main/java/com/example/administrator/practicaltraining/ActivityMessage.java
@@ -11,18 +11,22 @@ import android.widget.RelativeLayout;
  */
 public class ActivityMessage extends Activity {
     private RelativeLayout relativeLayout;
+    private RelativeLayout relativeLayout1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message);
-        relativeLayout=(RelativeLayout)findViewById(R.id.layout_shake);
+        relativeLayout = (RelativeLayout) findViewById(R.id.layout_shake);
         relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.setClass(ActivityMessage.this,ActivitySignin.class);
+                Intent intent = new Intent();
+                intent.setClass(ActivityMessage.this, ActivitySignin.class);
                 startActivity(intent);
             }
         });
+        relativeLayout1 = (RelativeLayout) findViewById(R.id.layout_QR);
+
     }
 }
--- app/src/main/java/com/example/administrator/practicaltraining/ActivitySign.java
+++ app/src/main/java/com/example/administrator/practicaltraining/ActivitySign.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2016/11/29.
@@ -14,6 +17,10 @@ import android.widget.ImageView;
 public class ActivitySign extends AppCompatActivity {
     public ImageView back;
     private Button button;
+    private EditText editText;
+    private EditText editText1;
+    private EditText editText2;
+    private RadioButton radioButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +34,21 @@ public class ActivitySign extends AppCompatActivity {
             }
         });
         Button button=(Button)findViewById(R.id.id_btn);
+        editText=(EditText)findViewById(R.id.truename) ;
+        editText1=(EditText)findViewById(R.id.year) ;
+        editText2=(EditText)findViewById(R.id.mymoblenumber);
+        final String txt=editText.getText().toString();
         button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent();
-                intent.setClass(ActivitySign.this,Activityinformsuccessful.class);
-                startActivity(intent);
+                if (txt.length() == 0) {
+                    Toast.makeText(ActivitySign.this, "请输入真实姓名", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent();
+                    intent.setClass(ActivitySign.this, Activityinformsuccessful.class);
+                    startActivity(intent);
+                }
             }
         });
     }
