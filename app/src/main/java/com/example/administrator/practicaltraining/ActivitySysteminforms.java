658abcab8fecae3911376cbc93cc982c780239ee
weisongsongss <15231167995@139.com>
2016-12-05 21:16:21 +0800

    修改了发布兼职界面的Radiobutton的问题，增加了系统通知的返回按钮

--- app/src/main/java/com/example/administrator/practicaltraining/ActivitySysteminforms.java
+++ app/src/main/java/com/example/administrator/practicaltraining/ActivitySysteminforms.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.app.ListActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -11,13 +13,18 @@ import java.util.List;
 import java.util.Map;
 
 public class ActivitySysteminforms extends Activity {
-
-
+    public ImageView back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_systeminforms);
-
+        back = (ImageView)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
