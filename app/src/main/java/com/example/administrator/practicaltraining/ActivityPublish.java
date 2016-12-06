af94960907d33fd570f090e71792038fc1c0249c
weisongsongss <15231167995@139.com>
2016-12-06 10:47:31 +0800

    优化了发布兼职界面，增加了发布兼职的点击事件

--- app/src/main/java/com/example/administrator/practicaltraining/ActivityPublish.java
+++ app/src/main/java/com/example/administrator/practicaltraining/ActivityPublish.java
@@ -1,9 +1,14 @@
 package com.example.administrator.practicaltraining;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 /**
  * Created by Administrator on 2016/11/30.
@@ -11,6 +16,7 @@ import android.widget.ImageView;
 
 public class ActivityPublish extends Activity {
     public ImageView back;
+    public Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,26 @@ public class ActivityPublish extends Activity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        btn = (Button)findViewById(R.id.id_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityPublish.this);
+                builder.setTitle("发布提示：");
+                builder.setMessage("确定要发布兼职吗？");
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast toast = Toast.makeText(ActivityPublish.this,"信息已提交，请注意查收通知。",Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER,0,0);
+                        toast.show();
+                        ActivityPublish.this.finish();
+                    }
+                });
+                builder.setNegativeButton("取消",null);
+                builder.create();
+                builder.show();
             }
         });
     }
