5f7891c223981750d927dc5bd7b3ced5b993c257
liutong <707734225@qq.com>
2016-12-08 09:35:15 +0800

    优化注册页

--- app/src/main/java/com/example/administrator/practicaltraining/Resetpwd.java
+++ app/src/main/java/com/example/administrator/practicaltraining/Resetpwd.java
@@ -1,6 +1,10 @@
 package com.example.administrator.practicaltraining;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Resetpwd extends AppCompatActivity {
 
@@ -8,5 +12,15 @@ public class Resetpwd extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpwd);
+        Button btn = (Button) findViewById(R.id.register_btn_cancel);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(Resetpwd.this, ActivityLogin.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
